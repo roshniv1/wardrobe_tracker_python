3e8ab07fc6f2545d42760515873d49d0a1d9496e
Roshni Vincent <roshni-vincent@hotmail.com>
2020-02-29 15:51:21 +0000

    Prevent duplicate wardrobe items

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-""" This code creates a basic wardrobe tracker app using the Kivy package. Currently functionality includes the ability
-to add and remove items and updating the database. Planned upgrades include adding different wardrobe categories,
-checking for duplicate items and adding photos.
+"""
+This code creates a basic wardrobe tracker app using the Kivy package. Currently functionality includes the ability
+to add and remove items and updating the database. Planned upgrades include adding different wardrobe categories and
+photos.
 """
 
 from kivy.uix.screenmanager import Screen
@@ -36,12 +37,10 @@ ViewWindow:
 class ListItemWithIcon(OneLineAvatarIconListItem):
     # Popup to confirm delete
     def confirm_dialog(self):
-        # create instance
-        print([self.text])
-        my_dialog = MDDialog(title="Delete Confirmation", text="Are you sure you want to delete '" + self.text + "'?",
-                             auto_dismiss=False, events_callback=self.remove_item, text_button_ok="Delete",
-                             text_button_cancel="Cancel", size_hint=[0.5, 0.5])
-        my_dialog.open()
+        confirm_dialog = MDDialog(title="Delete Confirmation", text="Are you sure you want to delete '" + self.text + "'?",
+                                  auto_dismiss=False, events_callback=self.remove_item, text_button_ok="Delete",
+                                  text_button_cancel="Cancel", size_hint=[0.5, 0.5])
+        confirm_dialog.open()
 
     # Remove item from widget and table
     def remove_item(self, selection_text, popup_widget):
@@ -57,17 +56,23 @@ class ListItemWithIcon(OneLineAvatarIconListItem):
 
 class ViewWindow(Screen):
     # Popup to add wardrobe item
-    def input_dialog(self):
-        my_dialog = MDInputDialog(title="Add Item", hint_text="Type new wardrobe item", text_button_ok="Add",
-                                  events_callback=self.add_item, size_hint=[0.5, 0.5])
-        my_dialog.open()
+    def input_dialog(self, *args):
+        input_dialog = MDInputDialog(title="Add Item", hint_text="Type new wardrobe item", text_button_ok="Add",
+                                     events_callback=self.add_item, size_hint=[0.5, 0.5])
+        input_dialog.open()
 
     # Adds item to widget and table
     def add_item(self, selection_text, popup_widget):
-        self.ids.container.add_widget(Factory.ListItemWithIcon(text=popup_widget.text_field.text))
         con = sql.connect("demo.db")
         cur = con.cursor()
-        cur.execute("""INSERT INTO Wardrobe (item) VALUES (?)""", [popup_widget.text_field.text])
+        cur.execute("""SELECT * FROM Wardrobe WHERE item = (?)""", [popup_widget.text_field.text])
+        if cur.fetchone() is None:
+            self.ids.container.add_widget(Factory.ListItemWithIcon(text=popup_widget.text_field.text))
+            cur.execute("""INSERT INTO Wardrobe (item) VALUES (?)""", [popup_widget.text_field.text])
+        else:
+            dup_dialog = MDDialog(title="Existing Item", text="This item already exists", auto_dismiss=True,
+                                  text_button_ok="Go Back", events_callback=self.input_dialog, size_hint=[0.5, 0.5])
+            dup_dialog.open()
         con.commit()
         con.close()
 
@@ -84,15 +89,16 @@ class MyMainApp(MDApp):
         con = sql.connect("demo.db")
         cur = con.cursor()
         cur.execute("""CREATE TABLE IF NOT EXISTS Wardrobe(item text)""")
-        cur.execute("""SELECT * FROM WARDROBE""")
+        cur.execute("""SELECT * FROM Wardrobe""")
         rows = cur.fetchall()
 
-        # Add item to scrollview
+        # Add item to scroll view
         for row in rows:
             print(row[0])
             self.screen.ids.container.add_widget(
                 Factory.ListItemWithIcon(text=row[0])
             )
+
         con.commit()
         con.close()
 
